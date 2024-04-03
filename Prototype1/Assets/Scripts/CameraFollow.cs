5d1957eecf88314d7b2edb348ae37e24096f62be
thatonefoo <142627731+thatonefoo@users.noreply.github.com>
2024-04-03 13:09:22 -0700

    Update the Camera Follow to remove unused libraries, serialize public field and remove unused method

--- Prototype1/Assets/Scripts/CameraFollow.cs
+++ Prototype1/Assets/Scripts/CameraFollow.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
     //Vehicle game object to follow
-    public GameObject player;
+    [SerializeField] private GameObject player;
     //offset from the vehicle
     private Vector3 offset = new Vector3(0.0f, 12.0f, -10.0f);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     
+    
+       
 
     //Update just before the frame is rendered
     void LateUpdate()
