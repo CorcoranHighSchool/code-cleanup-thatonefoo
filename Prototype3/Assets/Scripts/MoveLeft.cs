4581e0c20f20e1846e97668683782557414ef1c3
thatonefoo <142627731+thatonefoo@users.noreply.github.com>
2024-04-08 13:21:54 -0700

    Removed unused Libaries. Converted Strings to constant. Optimized Vector3.

--- Prototype3/Assets/Scripts/MoveLeft.cs
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
@@ -10,10 +9,13 @@ public class MoveLeft : MonoBehaviour
     private PlayerController playerControllerScript;
     // Left bounds
     private float leftBound = -15.0f;
+    private const string player = 'Player;
+    private const string player = 'obstacle';
+    
     // Start is called before the first frame update
     void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find(player).GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class MoveLeft : MonoBehaviour
         //Move left as long as the game is not over
         if (playerControllerScript.gameOver == false)
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed * Time.deltaTime));
         }
 
         //Destroy this object if it is an obstacle and has gone too far
