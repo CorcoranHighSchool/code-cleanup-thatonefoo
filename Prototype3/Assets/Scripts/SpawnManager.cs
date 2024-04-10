256bf751948da43ab475ce7fb20a458f0f71001f
thatonefoo <142627731+thatonefoo@users.noreply.github.com>
2024-04-10 13:28:30 -0700

    everyhing is fixed

--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -1,2 +1,26 @@
 ﻿
+using UnityEngine;
 
+public class SpawnManager : MonoBehaviour
+{
+    //Prefab for the obstacle to spawn
+    [SerializeField] private GameObject obstaclePrefab;
+    //Spawn Position
+    private Vector3 spawnPos = new Vector3(25.0f, 0.0f, 0.0f);
+
+    //Spawn delay
+    private float spawnDelay = 2.0f;
+    //Spawn Rate
+    private float spawnRate = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        InvokeRepeating(SpawnObstacle, spawnDelay, spawnRate);
+    }
+    private void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}--
